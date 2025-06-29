9602f2dc6385920a0bdb8aba93ea13afbabf6b68
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-06-29 01:14:32 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/agendasystem/AgendaSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.agendasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/agendasystem/AgendaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agendasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
