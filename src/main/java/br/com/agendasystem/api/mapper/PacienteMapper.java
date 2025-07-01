9f7249f654152faba710f6412639d89ba36412e9
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-07-01 20:38:41 -0300

    refactor: ajusta métodos do PacienteMapper para usar ModelMapper na conversão

--- src/main/java/br/com/agendasystem/api/mapper/PacienteMapper.java
+++ src/main/java/br/com/agendasystem/api/mapper/PacienteMapper.java
@@ -3,32 +3,29 @@ package br.com.agendasystem.api.mapper;
 import br.com.agendasystem.api.request.PacienteRequest;
 import br.com.agendasystem.api.response.PacienteResponse;
 import br.com.agendasystem.domain.entity.Paciente;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class PacienteMapper {
 
-    public static Paciente toPaciente(PacienteRequest request) {
-        Paciente paciente = new Paciente();
-        paciente.setNome(request.getNome());
-        paciente.setSobrenome(request.getSobrenome());
-        paciente.setEmail(request.getEmail());
-        paciente.setCpf(request.getCpf());
-        return paciente;
+    private final ModelMapper modelMapper;
+
+    public Paciente toPaciente(PacienteRequest request) {
+        return modelMapper.map(request,Paciente.class);
     }
 
-    public static PacienteResponse toPacienteResponse(Paciente paciente) {
-        PacienteResponse pacienteResponse = new PacienteResponse();
-        pacienteResponse.setId(paciente.getId());
-        pacienteResponse.setNome(paciente.getNome());
-        pacienteResponse.setSobrenome(paciente.getSobrenome());
-        pacienteResponse.setEmail(paciente.getEmail());
-        pacienteResponse.setCpf(paciente.getCpf());
-        return pacienteResponse;
-
+    public PacienteResponse toPacienteResponse(Paciente paciente) {
+        return modelMapper.map(paciente, PacienteResponse.class);
     }
-
-
-
+    public List<PacienteResponse> toPacienteResponseList(List<Paciente> pacientes) {
+       return pacientes.stream()
+               .map(paciente -> modelMapper.map(paciente,PacienteResponse.class)).toList();
+    }
 
 }
 
