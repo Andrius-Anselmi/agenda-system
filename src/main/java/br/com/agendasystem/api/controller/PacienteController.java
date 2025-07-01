c3f19a91ffbb04726eff5e47b5e4ed6d0fcf52b6
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-07-01 20:45:22 -0300

    feat: implementa novos métodos no controller e atualiza, para usar métodos do mapper com o ModelMapper

--- src/main/java/br/com/agendasystem/api/controller/PacienteController.java
+++ src/main/java/br/com/agendasystem/api/controller/PacienteController.java
@@ -3,14 +3,17 @@ package br.com.agendasystem.api.controller;
 import br.com.agendasystem.api.mapper.PacienteMapper;
 import br.com.agendasystem.api.request.PacienteRequest;
 import br.com.agendasystem.api.response.PacienteResponse;
+import br.com.agendasystem.config.ModalMapperConfig;
 import br.com.agendasystem.domain.entity.Paciente;
 import br.com.agendasystem.domain.entity.service.PacienteService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/paciente")
@@ -18,16 +21,43 @@ import java.util.List;
 public class PacienteController {
 
     private final PacienteService service;
+    private final PacienteMapper mapper;
 
     @PostMapping()
     public ResponseEntity<PacienteResponse> salvar(@RequestBody PacienteRequest request) {
-        Paciente pacienteSalvo = service.salvar(PacienteMapper.toPaciente(request));
-        return ResponseEntity.status(HttpStatus.CREATED).body(PacienteMapper.toPacienteResponse(pacienteSalvo));
+        Paciente pacienteSalvo = service.salvar(mapper.toPaciente(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.toPacienteResponse(pacienteSalvo));
     }
 
     @GetMapping()
     public ResponseEntity<List<PacienteResponse>> listarTodos() {
         List<Paciente> pacientes = service.listarTodos();
-        return ResponseEntity.status(HttpStatus.OK).body(pacientes.stream().map(PacienteMapper::toPacienteResponse).toList());
+        return ResponseEntity.ok(mapper.toPacienteResponseList(pacientes));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteResponse> buscarPorId(@PathVariable Long id) {
+        Optional<Paciente> paciente = service.buscarPorId(id);
+        if(paciente.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(mapper.toPacienteResponse(paciente.get()));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PacienteResponse> atualizar(@PathVariable Long id, @RequestBody PacienteRequest request) {
+        Optional<Paciente> pacienteBuscado = service.buscarPorId(id);
+        if(pacienteBuscado.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        Paciente paciente = pacienteBuscado.get();
+        paciente.setNome(request.getNome());
+        paciente.setEmail(request.getEmail());
+        paciente.setCpf(request.getCpf());
+        paciente.setSobrenome(request.getSobrenome());
+
+        Paciente pacienteAtualizado = service.salvar(paciente);
+        return ResponseEntity.status(HttpStatus.OK).body(mapper.toPacienteResponse(pacienteAtualizado));
+
     }
 }
