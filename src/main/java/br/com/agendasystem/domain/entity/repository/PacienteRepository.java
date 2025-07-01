941ff1af1f09d422a019d2c85cb5b2ff2123fe7e
Andrius Anselmi <andriusanselmi12@gmail.com>
2025-06-30 23:39:57 -0300

    feat: adicionar interface repository para entidade Paciente

--- src/main/java/br/com/agendasystem/domain/entity/repository/PacienteRepository.java
+++ src/main/java/br/com/agendasystem/domain/entity/repository/PacienteRepository.java
@@ -3,5 +3,9 @@ package br.com.agendasystem.domain.entity.repository;
 import br.com.agendasystem.domain.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    Optional<Paciente> findByCpf(String cpf);
 }
